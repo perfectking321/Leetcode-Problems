99af1b0ad7d3f801c6175f3eed23e00231c46777
Iyad Ahmed <perfectkings123@gmail.com>
2026-01-27 07:14:25 +0530

    Time: 2 ms (98.8%), Space: 44.2 MB (82.35%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+class Solution {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right)
+        {
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+            if(!Character.isLetterOrDigit(l))
+            left++;
+            else if(!Character.isLetterOrDigit(r))
+            right--;
+            else if(Character.toLowerCase(l)!=Character.toLowerCase(r))
+            return false;
+
+            else
+            {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        boolean ans = isPalindrome(s);
+        if(ans) System.out.print("true");
+        else System.out.print("false");
+        scan.close();
+    }
+}
