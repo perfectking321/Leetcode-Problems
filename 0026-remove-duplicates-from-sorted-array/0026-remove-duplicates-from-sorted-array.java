edd5469c538cf42202ca85359359f763ac26b8ef
Iyad Ahmed <perfectkings123@gmail.com>
2026-01-28 05:51:03 +0530

    Time: 0 ms (100%), Space: 46.9 MB (33.9%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,17 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        //stores unique elemets which we seen
-        HashSet<Integer> seen = new HashSet<>();
-        //position to overwrite next unique element
-        int index=0;
-        for(int num: nums){
-            //unique elements
-            if(!seen.contains(num)){
-                seen.add(num);
-                nums[index] = num;
-                index++;
+        int i=0;
+        if(nums.length==0) return 0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
             }
         }
-        return index;
+        return i+1;
     }
 }
