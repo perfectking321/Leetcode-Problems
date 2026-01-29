0011ef4974deecbab36d6061624972fda870770f
Iyad Ahmed <perfectkings123@gmail.com>
2026-01-29 11:25:09 +0530

    Time: 0 ms (100%), Space: 47.2 MB (88.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,19 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        
-        // Check if 0 is missing
-        if(nums[0] != 0) return 0;
-        
-        // Check middle numbers
-        for(int i = 1; i < n; i++){
-            if(nums[i] != i){
-                return i;
-            }
+        int N = nums.length;
+        int sum = (N * (N + 1)) / 2;
+
+        // Calculate the sum of elements in the array
+        int actualSum = 0;
+        for (int i = 0; i < N; i++) {
+            actualSum += nums[i];
         }
-        
-        // If no number is missing in 0 to n-1, then n is missing
-        return n;
+
+        // Missing number is the difference
+        return sum - actualSum;
     }
 }
