9cc2506a4d4e45f803cef4b36240d6362e992f14
Iyad Ahmed <perfectkings123@gmail.com>
2026-02-09 07:11:11 +0530

    Time: 0 ms (100%), Space: 42.4 MB (10.74%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        // init dp with -1
+        for(int[] row:dp) Arrays.fill(row,-1);
+        return func(m-1,n-1,dp);
+    }
+    private int func(int i,int j, int[][] dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = func(i-1,j,dp);
+        int left = func(i,j-1,dp);
+        return dp[i][j]=up+left;
+    }
+}
