5146cebd454f93773b3693d761e04e7662ca8aca
Iyad Ahmed <perfectkings123@gmail.com>
2026-02-09 21:23:36 +0530

    Time: 7 ms (76.85%), Space: 44.3 MB (15.2%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] m = new int[256];
+        int[] n = new int[256];
+        for(int i=0;i<s.length();i++){
+            if(m[s.charAt(i)] != n[t.charAt(i)]) return false;
+            m[s.charAt(i)]=i+1;
+            n[t.charAt(i)]=i+1;
+        }
+        return true;
+    }
+}
