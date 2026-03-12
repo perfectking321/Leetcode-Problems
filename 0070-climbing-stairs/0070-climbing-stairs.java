ba99a5abf394d87cabbb13de6830598b77941100
Iyad Ahmed <perfectkings123@gmail.com>
2026-03-12 08:35:24 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        // base case
         dp[0]=1;
         dp[1]=1;
-        // run the loop
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<=n;i++)
+        {
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
